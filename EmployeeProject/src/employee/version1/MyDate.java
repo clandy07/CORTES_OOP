b9ca601905fd475581cd4d8619901e0d15c55811
clandy07 <127475702+clandy07@users.noreply.github.com>
2023-09-15 10:51:23 +0800

    EmployeeProject

---
+++ EmployeeProject/src/employee/version1/MyDate.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package employee.version1;
+
+/**
+ *
+ * @author User
+ */
+public class MyDate {
+    private int day;
+    private int month;
+    private int year;
+
+    public MyDate(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%02d:%02d:%d", this.day, this.month, this.year);
+    }   
+}
---
+++ EmployeeProject/src/employee/version1/PieceWorkerEmployee.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package employee.version1;
+
+/**
+ *
+ * @author User
+ */
+public class PieceWorkerEmployee {
+    private int empID;
+    private String empName;
+    private MyDate empDateHired;
+    private MyDate empBirthDate;
+    private int totalPiecesFinished;
+    private float ratePerPiece;
+    private float retVal;
+    private int temp;
+    
+     public PieceWorkerEmployee() {
+    }
+
+    public PieceWorkerEmployee(int empID, String empName, MyDate empDateHired, MyDate empBirthDate, int totalPiecesFinished, float ratePerPiece) {
+        this.empID = empID;
+        this.empName = empName;
+        this.empDateHired = empDateHired;
+        this.empBirthDate = empBirthDate;
+        this.totalPiecesFinished = totalPiecesFinished;
+        this.ratePerPiece = ratePerPiece;
+    }
+
+    public PieceWorkerEmployee(int empID, String empName, MyDate empDateHired, MyDate empBirthDate) {
+        this.empID = empID;
+        this.empName = empName;
+        this.empDateHired = empDateHired;
+        this.empBirthDate = empBirthDate;
+    }
+
+    public int getEmpID() {
+        return empID;
+    }
+
+    public void setEmpID(int empID) {
+        this.empID = empID;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public MyDate getEmpDateHired() {
+        return empDateHired;
+    }
+
+    public void setEmpDateHired(MyDate empDateHired) {
+        this.empDateHired = empDateHired;
+    }
+
+    public MyDate getEmpBirthDate() {
+        return empBirthDate;
+    }
+
+    public void setEmpBirthDate(MyDate empBirthDate) {
+        this.empBirthDate = empBirthDate;
+    }
+
+    public int getTotalPiecesFinished() {
+        return totalPiecesFinished;
+    }
+
+    public void setTotalPiecesFinished(int totalPiecesFinished) {
+        this.totalPiecesFinished = totalPiecesFinished;
+    }
+
+    public float getRatePerPiece() {
+        return ratePerPiece;
+    }
+
+    public void setRatePerPiece(float ratePerPiece) {
+        this.ratePerPiece = ratePerPiece;
+    }
+
+    public double computeSalary() {
+        if(this.totalPiecesFinished < 100) {
+            retVal = this.totalPiecesFinished * this.ratePerPiece;
+        }
+        else {
+           this.temp = this.totalPiecesFinished / 100;
+           this.retVal = (this.totalPiecesFinished * this.ratePerPiece) + (this.temp * (this.ratePerPiece * 10));
+           
+        }
+       
+        
+        return retVal;
+    }
+    
+    public void displayInfo() {
+        System.out.printf("Pieces finished: %d\nRate per piece: Php %.2f\nTotal income: Php %.2f",totalPiecesFinished,ratePerPiece,retVal);
+    }
+
+    @Override
+    public String toString() {
+        return "\n\nID: " + this.empID + "\nName: " + this.empName + "\nDate Hired: " + this.empDateHired + "\nBirth Date: " + this.empBirthDate;
+    }
+    
+    
+
+   
+    
+  
+}
